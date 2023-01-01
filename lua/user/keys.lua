546ea8544bd477a53fdb8726e7c21f4844650c43
Mark Dube <mjdube99@gmail.com>
2023-01-01 12:37:20 -0600

    moved options/keys to their own file

---
+++ lua/user/keys.lua
@@ -0,0 +1,40 @@
+-- keymappings
+lvim.leader = "space"
+vim.g.mapleader = " "
+
+-- Get rid of lvim implementation of S-l and S-h
+lvim.keys.normal_mode["<S-l>"] = nil
+lvim.keys.normal_mode["<S-h>"] = nil
+
+vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<F1>", "<Nop>")
+vim.keymap.set("n", "<TAB>", "<cmd>bnext<CR>")
+vim.keymap.set("n", "<S-TAB>", "<cmd>bprev<CR>")
+-- back space to switch to alternative buffer with the cursor in the last position it was in the file
+vim.keymap.set("n", "<bs>", "<bs> <c-^>`”zz")
+vim.keymap.set("n", "gv", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>")
+vim.keymap.set("n", "gA", "<cmd>lua vim.lsp.codelens.run()<cr>")
+
+-- repeat macros with .
+vim.api.nvim_set_keymap("x", ".", ":normal .<CR>", { silent = true })
+
+vim.keymap.set("i", ",", ",<C-g>u")
+vim.keymap.set("i", ".", ".<C-g>u")
+vim.keymap.set("i", "!", "!<C-g>u")
+vim.keymap.set("i", "?", "?<C-g>u")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "Q", "<nop>")
---
+++ lua/user/options.lua
@@ -0,0 +1,10 @@
+-- Neovim settings
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.timeoutlen = 100
+vim.opt.mouse = ""
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.inccommand = "split" -- shows small split at the bottom of the screen when searching
+vim.opt.foldlevel = 4
+vim.opt.colorcolumn="80"
