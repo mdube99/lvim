3e79e3fffb8d699465d5caaca75750b2c6508235
Mark Dube <mjdube99@gmail.com>
2023-01-02 14:32:00 -0600

    removed unused plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -10,7 +10,6 @@
     "vimwiki/vimwiki",
       -- ft = {"markdown"},
     },
-    { "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, },
     { "tpope/vim-fugitive" },
     { "tpope/vim-surround" },
     { "lambdalisue/suda.vim" },
